acf1c52a3f71e70ce0b9cad17a81570551db92a6
muhammadnaeemmadni <muhammadnaeemmadni786@gmail.com>
2021-03-25 11:20:57 +0500

    Bisma-gatsby-portfolio

---
+++ rolwinreevan_gatsby_blog/src/components/PageFragments/HomePage/AboutMe.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import AboutTile from '../../AbouTile';
+import { stripTags, domHtml } from '../../../utils/stripTags';
+
+import SEO from '../../Seo';
+
+const pageText = {
+  paraOne: `Hello !! My name is Bisma Touseef. I'm a web developer who is
+    passionate about various web technologies. I'm a student of BsCS at Virtual University .
+    Currently I work mostly with Javascript technologies like ReactJS and NodeJS .`,
+  paraTwo: ` I'm also a <b>Graphic 
+  Designer.</b> Currently i'm also doing freelancing.
+   I'm always a learner and a self taught programmer and graphic designer.`,
+};
+
+const AboutMe = () => {
+  const description = `${pageText.paraOne} ${stripTags(pageText.paraTwo)}`;
+  return (
+    <>
+      <div>
+        <SEO
+          title="About"
+          description={description}
+          path=""
+          keywords={['Rolwin', 'Reevan', 'Monteiro', 'FullStack developer', 'Javascript', 'ReactJS', 'NodeJS', 'Gatsby']}
+        />
+        <h1 className="titleSeparate">About Me</h1>
+        <p>
+          {pageText.paraOne}
+        </p>
+        <p dangerouslySetInnerHTML={domHtml(pageText.paraTwo)} />
+      </div>
+      <Row gutter={[20, 20]}>
+        <Col xs={24} sm={24} md={12} lg={8}>
+          <AboutTile
+            img="location.png"
+            height={60}
+            alt="location image"
+            textH4="Born and bought up in"
+            textH3="Karachi, Pakistan"
+          />
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8}>
+          <AboutTile
+            img="coffee.png"
+            alt="coffee image"
+            textH4="Love Coffee"
+            textH3="Coffee + Me = Happiness"
+          />
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8}>
+          <AboutTile
+            img="meeting.png"
+            alt="meeting image"
+            textH4="Socially adept"
+            textH3="Always"
+          />
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8}>
+          <AboutTile
+            img="cute.jpg"
+            alt="Book image"
+            textH4="Love Reading"
+            textH3="Reading is Love"
+          />
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8}>
+          <AboutTile
+            img="web.png"
+            alt="web image"
+            textH4="Self Taught Programmer"
+            textH3="Thanks to the Web Resources"
+            height={60}
+            width={60}
+          />
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8}>
+          <AboutTile
+            img="graduation.png"
+            alt="graduation image"
+            textH4="Doing B.Tech in"
+            textH3="Computer Science"
+            height={60}
+            width={60}
+          />
+        </Col>
+      </Row>
+    </>
+  );
+};
+export default AboutMe;
